c32c732ca90bb8184f1496db94ffbc3305ad116a
javierrcyb <jsr38819@email.vcss.edu>
2025-07-17 16:57:47 -0500

    front-end deploy changes

--- back-end/app.js
+++ back-end/app.js
@@ -3,43 +3,32 @@ const cors = require('cors');
 const session = require('express-session');
 const passport = require('passport');
 const configurePassport = require('./passport/config');
-const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
-// CORS solo en desarrollo
-if (process.env.NODE_ENV !== 'production') {
-  app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-  }));
-}
+const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 
 app.use(express.json());
-
 app.use(session({
   secret: 'supersecreto',
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false } // Usa true si usas HTTPS
+  cookie: { secure: false }
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 configurePassport(passport);
 
-// Rutas API
+app.get('/', (req, res) => res.send('API ReThinkBot working'));
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/conversations', require('./routes/conversation'));
-
-// === Servir frontend ===
-const clientPath = path.join(__dirname, '../rethinkbot-client/dist');
-app.use(express.static(clientPath));
-
-// Fallback: SPA
-app.get(/^\/(?!api\/).*/, (req, res) => {
-  res.sendFile(path.join(clientPath, 'index.html'));
-});
 
 module.exports = app;
